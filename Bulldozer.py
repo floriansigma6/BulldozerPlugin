9d471fa3afd18da1274e6a0a1386fc60b53e66f2
Pavel R <bugpwr@gmail.com>
2019-02-03 00:37:17 +0200

    Update Bulldozer.py

--- Bulldozer.py
+++ Bulldozer.py
@@ -1,16 +1,15 @@
 # Cura PostProcessingPlugin
 # Author:   Pavel Radzivilovsky
-# Date:     Frbryary 2, 2019
+# Date:     February 2, 2019
 
 # Description:  !!! Use at your own risk !!!
-#				This plugin creates gcode that can seriously damage your printer.
+#                This plugin creates gcode that can seriously damage your printer.
 #               Inserts code to push the print out of the printer, and restart the print of a new object.
 #               Can be used to automate production process of multiple objects. Never use unattended!
-#				The model must be centered horizontally and placed in the front 2/3 of the build plate.
-#				Tested with Ultimaker 3. And seriously, use at your own risk, this can break your printer. 
+#                The model must be centered horizontally and placed in the front 2/3 of the build plate.
+#                Tested with Ultimaker 3. And seriously, use at your own risk, this can break your printer. 
 
 from ..Script import Script
-from UM.Application import Application
 
 class Bulldozer(Script):
     def __init__(self):
@@ -18,7 +17,7 @@ class Bulldozer(Script):
 
     def getSettingDataString(self):
         return """{
-            "name": "Use print head to kick the model out of the printer",
+            "name": "Bulldozer - Use print head to kick the model out of the printer",
             "key": "Bulldozer",
             "metadata": {},
             "version": 1,
@@ -28,45 +27,46 @@ class Bulldozer(Script):
                 {
                     "label": "Cooling delay",
                     "description": "Cooling time before push. Recommended at least 20 minutes",
-                    "unit": "mm",
+                    "unit": "min",
                     "type": "int",
                     "default_value": 20
-                }
-				"parts"
-				{
-					"label": "Repeat",
+                },
+                "parts"
+                {
+                    "label": "Repeat",
                     "description": "Number of builds to perform",
                     "type": "int",
-                    "default_value": 5					
-				}
+                    "default_value": 5                    
+                }
             }
         }"""
-	
-	def dozerCode(imodel):
-		code = """\n\n
+    
+    def dozerCode(imodel):
+        code = """
 ;Bulldozer code start
 G0 F2000 Z150
 G0 F2000 Y210 Z150
 G0 F2000 X105 Y210 Z150"""
-		for m in range(delay):
-			for s in range(6):
-				code += "G4 P10000\nM117 Cooling down, " + str(delay-m) + ":" + str(5-s) + "0\n";
-				
-		code += """M117 Lo and behold!\n
+
+        for m in range(delay):
+            for s in range(6):
+                code += "G4 P10000\nM117 Cooling down, " + str(delay-m) + ":" + str(5-s) + "0\n"
+                
+        code += """M117 Lo and behold!\n
 G0 F2000 X105 Y210 Z5
 G0 F2000 X105 Y5 Z5   ; bulldozer
 M117 Starting model 
-""" + str(imodel+1+1) + "/" + str(parts) + "\n\n";		# +1 for 1-based numbers for human user, +1 coz starting next model after this one
-		
-		return code
-		
+""" + str(imodel+1+1) + "/" + str(parts) + "\n\n"        # +1 for 1-based numbers for human user, +1 coz starting next model after this one
+        
+        return code
+        
     
     def execute(self, data):
-		delay = self.getSettingValueByKey("delay")
-		parts = self.getSettingValueByKey("parts")
+        delay = self.getSettingValueByKey("delay")
+        parts = self.getSettingValueByKey("parts")
 
-		index = 0
-		
+        index = 0
+        
         RepeatedPart = ""
         StartCod = 0
         StopCod = 0
@@ -80,50 +80,24 @@ M117 Starting model
                 if line.startswith("G280"):
                     line = "G0 F15000 X" + str(g280x) + " Y6 Z2\nG280 ;position moved\n"
                     g280x -= 30
-				
-                if 	line == ";End of Gcode":
+                
+                if     line == ";End of Gcode":
                     line = ""
-                    for i in range(parts-1): 	
-                        line += dozerCode(i)
+                    for i in range(parts-1):     
+                        line += self.dozerCode(i)
                         line += RepeatedPart
-                    line += dozerCode(parts-1)
+                    line += self.dozerCode(parts-1)
                     line += ";End of Gcode \n"
                     StopCod=1
-					
+                    
                 if (StartCod==1) and (StopCod==0):
                     RepeatedPart += line + "\n"
-					
-                if 	line == ";END_OF_HEADER":
+                    
+                if     line == ";END_OF_HEADER":
                     StartCod=1
-					
+                    
                 modified_gcode += line + "\n"
 
             data[index] = modified_gcode
-            index += 1
-
-
-#        lcd_text = "M117 " + name + " layer: "
-#        i = 0
-#		
-#		g280x = 140; 	# any number of G280 commands will be 
-#		
- #       for layer in data:
-	#		while -1 != layer.find("\nG280\n"):		
-	#			layer = layer.replace("\nG280\n", "\nG0 F15000 X" + str(g280x) + " Y6 Z2\nG280 ;position moved\n", 1)
-	#			g280x -= 30
-				
-			# the new coordinates of hirbunim - 140 and 110, to make them fall off when the part is pushed.
-		
-	#		i++
-#            display_text = lcd_text + str(i)
-#            layer_index = data.index(layer)
-#            lines = layer.split("\n")
-#            for line in lines:
-#                if line.startswith(";LAYER:"):
-#                    line_index = lines.index(line)
-#                    lines.insert(line_index + 1, display_text)
-#                    i += 1
-#            final_lines = "\n".join(lines)
-#            data[layer_index] = final_lines
-            
+            index += 1            
         return data
